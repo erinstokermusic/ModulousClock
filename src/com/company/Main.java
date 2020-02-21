4b89dd09c15434a690e55c4ef780892273194461
Erin Stoker <erinstokermusic@gmail.com>
2020-02-21 11:55:17 -0500

    UserDataCaptured

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+    }
+
+    public static void modulusClock() {
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+        int hour;
+        int minute;
+
+        System.out.println("Give me the hour.");
+        hour = keyboard.nextInt();
+        System.out.println("Give me the minutes.");
+        minute = keyboard.nextInt();
+
     }
 }
